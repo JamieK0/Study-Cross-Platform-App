fbc0ccbc08ea7bb699060857994848bc215a1cdb
j14273 <62167467+j14273@users.noreply.github.com>
2024-01-10 23:06:55 +1100

    Working nav menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:study/pages/first_page.dart';
+import 'package:study/pages/home_page.dart';
+import 'package:study/pages/settings_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +17,11 @@ List names = ["jamie", "nish", "henry"];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: FirstPage(),
+      routes: {
+        '/firstpage' : (context) => FirstPage(),
+        '/homepage' : (context) => HomePage(),
+        '/settingspage' :(context) => SettingsPage()
+      }
       );
   }
 }
--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:study/pages/second_page.dart';
+import 'package:study/pages/home_page.dart';
 
 class FirstPage extends StatelessWidget {
   const FirstPage({super.key});
@@ -7,6 +7,33 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Colors.blue[200],
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Icon(
+                  Icons.egg,
+                  size: 48
+                  ),
+                ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                onTap: () {
+                  Navigator.pushNamed(context, '/homepage');
+                }
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Settings"),
+                onTap: () {
+                  Navigator.pushNamed(context, '/settingspage');
+                }
+              )
+          ]
+        )
+      ),
       appBar: AppBar(
         title: Text("Page 1"),
         backgroundColor: Colors.blue[100],),
@@ -14,12 +41,7 @@ class FirstPage extends StatelessWidget {
           child: ElevatedButton(
             child: Text("Go to second page"),
             onPressed: () {
-              Navigator.push(
-                context, 
-                MaterialPageRoute(
-                  builder: (context) => SecondPage()
-                  ),
-                );
+              Navigator.pushNamed(context, '/secondPage');
             },
           )
         )
